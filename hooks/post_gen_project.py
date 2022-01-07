84e1912e4ed7a7e1c3a982d409bb491fb2025d7e
Mark Lee <67784750+MARKLEESP@users.noreply.github.com>
2022-01-07 15:01:44 +0000

    Update post_gen_project.py
    Ciaran, can we remove the conda environment hook here, please? I'd like the option to tailor the conda package selection before installing. Thanks,

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -8,4 +8,4 @@ subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 
 # * Create new conda environment
 # TODO Check if it exists already!
-subprocess.call(["conda", "env", "create", "-f", "environment.yml"])
+#subprocess.call(["conda", "env", "create", "-f", "environment.yml"])
